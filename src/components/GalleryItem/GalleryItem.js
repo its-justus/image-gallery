a6d549b25c6012bc0946b9faee9161c0e1c85e5a
its-justus <ianjohnson9042@gmail.com>
2020-06-14 13:50:14 -0500

    galleryItem rough format complete

--- src/components/GalleryItem/GalleryItem.js
+++ src/components/GalleryItem/GalleryItem.js
@@ -1,16 +1,29 @@
 import React from "react";
+import axios from "axios";
 
 class GalleryItem extends React.Component {
 	state = {
 		likes: this.props.image.likes
 	};
 
+	addLike = () => {
+		axios.put(`/gallery/like/${this.props.image.id}`)
+			.then((response) => {
+				console.log("Put success!", this.props.image.id);
+				this.setState({likes: this.state.likes + 1});
+			})
+			.catch((error) => {
+				console.log(error);
+			})
+	}
+
 	render() {
 		const {image} = this.props;
-		console.log(this.state);
 		return (
 			<div className="gallery-item">
-				<img src={image.path} alt={image.description} />
+				<img src={image.path} alt={image.description} /> <br/>
+				<button type="button" onClick={this.addLike}>Love it!</button> <br/>
+				<>{this.state.likes} people love this!</>
 			</div>
 		);
 	}
