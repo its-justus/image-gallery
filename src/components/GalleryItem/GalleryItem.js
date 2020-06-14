8a065401003d49957e9f9c1689f1cd966b8c2f86
its-justus <ianjohnson9042@gmail.com>
2020-06-14 14:04:37 -0500

    toggleDescription working properly

--- src/components/GalleryItem/GalleryItem.js
+++ src/components/GalleryItem/GalleryItem.js
@@ -2,32 +2,49 @@ import React from "react";
 import axios from "axios";
 
 class GalleryItem extends React.Component {
-	state = {
-		likes: this.props.image.likes
-	};
+  state = {
+    likes: this.props.image.likes,
+    showDescription: false,
+  };
 
-	addLike = () => {
-		axios.put(`/gallery/like/${this.props.image.id}`)
-			.then((response) => {
-				console.log("Put success!", this.props.image.id);
-				this.setState({likes: this.state.likes + 1});
-			})
-			.catch((error) => {
-				console.log(error);
-			})
-	}
+  addLike = () => {
+    axios
+      .put(`/gallery/like/${this.props.image.id}`)
+      .then((response) => {
+        console.log("Put success!", this.props.image.id);
+        this.setState({ likes: this.state.likes + 1 });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-	render() {
-		const {image} = this.props;
-		return (
-			<div className="gallery-item">
-				<img src={image.path} alt={image.description} /> <br/>
-				<button type="button" onClick={this.addLike}>Love it!</button> <br/>
-				<>{this.state.likes} people love this!</>
-			</div>
-		);
-	}
+  toggleDescription = () => {
+    this.setState({ showDescription: !this.state.showDescription });
+  };
+
+  render() {
+    const { image } = this.props;
+    return (
+      <div className="gallery-item">
+        {this.state.showDescription ? (
+          <p onClick={this.toggleDescription}>{image.description}</p>
+        ) : (
+          <img
+            src={image.path}
+            alt={image.description}
+            onClick={this.toggleDescription}
+          />
+        )}
+        <br />
+        <button type="button" onClick={this.addLike}>
+          Love it!
+        </button>{" "}
+        <br />
+        <>{this.state.likes} people love this!</>
+      </div>
+    );
+  }
 }
-
 
 export default GalleryItem;
