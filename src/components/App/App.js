28264c709b13d26bb925afbcaad3d827bb4f2fa1
its-justus <ianjohnson9042@gmail.com>
2020-06-14 12:09:39 -0500

    added GalleryList component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import GalleryList from "../GalleryList/GalleryList";
 
 class App extends Component {
   render() {
@@ -8,9 +9,9 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Gallery of my life</h1>
         </header>
-        <br/>
-        <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
+        <br />
+        <GalleryList />
+        <img src="images/goat_small.jpg" />
       </div>
     );
   }
---
+++ src/components/GalleryList/GalleryList.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class GalleryList extends React.Component {
+  state = {};
+
+  render() {
+    return (
+      <div className="gallery-list">
+        <p>Items go here</p>
+      </div>
+    );
+  }
+}
+
+export default GalleryList;
