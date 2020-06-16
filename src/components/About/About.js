6de57c885b56bcffc11289cf10d78c7b9cf6feae
its-justus <ianjohnson9042@gmail.com>
2020-06-15 22:34:01 -0500

    react router working properly. whether it's done right or not is up for debate

---
+++ src/components/About/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Typography } from '@material-ui/core';
+
+const About = (props) => {
+	return(
+	<Typography>
+		Gallery of My Life is all about the pictures of my life. Clearly you can see I like fast aircraft and cute puppies. Thank you for stopping by.
+	</Typography>
+)}
+
+export default About;
